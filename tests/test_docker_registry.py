69cb771c65b78968d9ddcb9f6e027a12824e6f7a
Cory Johns <johnsca@gmail.com>
2020-04-13 16:24:41 -0400

    Refactor to use charms.unit_test

--- tests/test_docker_registry.py
+++ tests/test_docker_registry.py
@@ -1,15 +1,3 @@
-import pytest
-from unittest import mock
-
-
-def patch_fixture(patch_target):
-    @pytest.fixture()
-    def _fixture():
-        with mock.patch(patch_target) as m:
-            yield m
-    return _fixture
-
-
 def test_imports():
     # dummy test to just make sure files import properly
     # replace with real tests later
