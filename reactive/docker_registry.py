1d90352fac8edc20f9d2b9a93a5097f6ee79ab23
Adam Dyess <adam.dyess@canonical.com>
2021-12-02 13:33:05 -0600

    Address LP#1950501 for cert-provider departed during configure_client (#50)
    * Address LP#1950501 for cert-provider departed during configure_client
    
    * Incorporate review comments, check endpoint.docker-registry.joined flag in remove_certs

--- reactive/docker_registry.py
+++ reactive/docker_registry.py
@@ -8,6 +8,7 @@ from charms.reactive import (
     set_flag,
     clear_flag,
     is_flag_set,
+    all_flags_set,
     when,
     when_any,
     when_not,
@@ -228,7 +229,8 @@ def remove_certs():
     layer.docker_registry.start_registry()
 
     # If we have clients, let them know our tls data has changed
-    if (is_flag_set('charm.docker-registry.client-configured')):
+    if all_flags_set('charm.docker-registry.client-configured',
+                     'endpoint.docker-registry.joined'):
         configure_client()
     report_status()
 
