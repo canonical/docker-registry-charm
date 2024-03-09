550c76a9fc9e223194922ae9f56a9782b6c794c3
Kevin W Monroe <kevin.monroe@canonical.com>
2024-03-09 13:39:24 -0600

    unit test storage-cache config (gotta start somewhere)

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,4 +1,4 @@
 import charms.unit_test
 
 charms.unit_test.patch_reactive()
-charms.unit_test.patch_module('charms.leadership')
+charms.unit_test.patch_module("charms.leadership")
--- tests/unit/test_docker_registry.py
+++ tests/unit/test_docker_registry.py
@@ -1,4 +1,5 @@
 from charms.unit_test import patch_fixture
+from unittest import mock
 
 from charmhelpers.core import host  # patched
 from charms import layer
@@ -6,8 +7,8 @@ from charms import layer
 from reactive import docker_registry as handlers
 
 
-start_registry = patch_fixture('charms.layer.docker_registry.start_registry')
-stop_registry = patch_fixture('charms.layer.docker_registry.stop_registry')
+start_registry = patch_fixture("charms.layer.docker_registry.start_registry")
+stop_registry = patch_fixture("charms.layer.docker_registry.stop_registry")
 
 
 def test_series_upgrade(start_registry, stop_registry):
@@ -28,3 +29,25 @@ def test_series_upgrade(start_registry, stop_registry):
     assert host.service_pause.call_count == 1
     assert host.service_resume.call_count == 1
     assert layer.status.blocked.call_count == 1
+
+
+@mock.patch("charms.layer.docker_registry._configure_local_client")
+@mock.patch("charms.layer.docker_registry.host")
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry.unitdata")
+@mock.patch("charmhelpers.core.hookenv.config")
+@mock.patch("os.makedirs", mock.Mock(return_value=0))
+def test_configure_registry(config, mock_kv, mock_write, mock_host, mock_lc):
+    config.return_value = {
+        "log-level": "bananas",
+        "storage-cache": "bananas",
+    }
+    expected = {
+        "log": {"level": "bananas"},
+        "storage": {"cache": {"blobdescriptor": "bananas"}},
+    }
+
+    with mock.patch("charms.layer.docker_registry.yaml") as mock_yaml:
+        layer.docker_registry.configure_registry()
+        args, _ = mock_yaml.safe_dump.call_args_list[0]
+        assert expected["storage"].items() <= args[0]["storage"].items()
