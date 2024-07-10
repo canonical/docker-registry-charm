3613f2f271476eb283d6e1ce72c4dbaf53e6f545
Alex Ramanau <aliaksei.ramanau@canonical.com>
2024-07-10 19:00:31 +0200

    Added s3 storage support
    All settings are described in distribution specification:
    https://distribution.github.io/distribution/storage-drivers/s3/
    
    Now registry users may choose one of 3 storages: filesystem, swift, s3.
    `filesystem` storage is the default option.

--- tests/unit/test_docker_registry.py
+++ tests/unit/test_docker_registry.py
@@ -76,3 +76,153 @@ def test_has_invalid_config(config):
         "storage-cache": "bananas",
     }
     assert "storage-cache" in layer.docker_registry.has_invalid_config()
+
+
+@mock.patch("os.makedirs", mock.Mock(return_value=0))
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry._configure_local_client")
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry.unitdata")
+@mock.patch("charmhelpers.core.hookenv.config")
+def test_configure_registry_s3_storage_smoke(config, *args):
+    config.return_value = {
+        "log-level": "info",
+        "storage-s3-region": "ns1",
+        "storage-s3-bucket": "test_bucket",
+    }
+    expected_storage = {
+        "s3": {
+            "bucket": "test_bucket",
+            "region": "ns1",
+            # "regionendpoint": "https://ns1-region.internal",
+            "forcepathstyle": False,
+            "encrypt": False,
+            "secure": True,
+            "skipverify": False,
+            "v4auth": True,
+            "chunksize": 10485760,
+            "multipartcopychunksize": 31457280,
+            "multipartcopymaxconcurrency": 100,
+            "multipartcopythresholdsize": 31457280,
+            "storageclass": "STANDARD",
+            "usedualstack": False,
+            "accelerate": False,
+            "loglevel": "off",
+        },
+    }
+    with mock.patch("charms.layer.docker_registry.yaml") as mock_yaml:
+        layer.docker_registry.configure_registry()
+        args, _ = mock_yaml.safe_dump.call_args_list[0]
+        assert 'storage' in args[0]
+        assert 's3' in args[0]['storage']
+        actual_storage_config = args[0]['storage']['s3']
+        assert expected_storage['s3'].items() == actual_storage_config.items()
+
+
+
+@mock.patch("os.makedirs", mock.Mock(return_value=0))
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry._configure_local_client")
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry.unitdata")
+@mock.patch("charmhelpers.core.hookenv.config")
+def test_configure_registry_s3_storage_region_endpoint(config, *args):
+    config.return_value = {
+        "log-level": "info",
+        "storage-s3-region": "ns1",
+        "storage-s3-regionendpoint": "https://ns1-region.internal",
+        "storage-s3-bucket": "test_bucket",
+    }
+    expected_storage = {
+        "s3": {
+            "bucket": "test_bucket",
+            "region": "ns1",
+            "regionendpoint": "https://ns1-region.internal",
+            "forcepathstyle": False,
+            "encrypt": False,
+            "secure": True,
+            "skipverify": False,
+            "v4auth": True,
+            "chunksize": 10485760,
+            "multipartcopychunksize": 31457280,
+            "multipartcopymaxconcurrency": 100,
+            "multipartcopythresholdsize": 31457280,
+            "storageclass": "STANDARD",
+            "usedualstack": False,
+            "accelerate": False,
+            "loglevel": "off",
+        },
+    }
+    with mock.patch("charms.layer.docker_registry.yaml") as mock_yaml:
+        layer.docker_registry.configure_registry()
+        args, _ = mock_yaml.safe_dump.call_args_list[0]
+        assert 'storage' in args[0]
+        assert 's3' in args[0]['storage']
+        actual_storage_config = args[0]['storage']['s3']
+        assert expected_storage['s3'].items() == actual_storage_config.items()
+
+
+@mock.patch("os.makedirs", mock.Mock(return_value=0))
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry._configure_local_client")
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry.unitdata")
+@mock.patch("charmhelpers.core.hookenv.config")
+def test_configure_registry_s3_storage_override_default(config, *args):
+    config.return_value = {
+        "log-level": "info",
+        "storage-s3-region": "ns1",
+        "storage-s3-bucket": "test_bucket",
+        "storage-s3-forcepathstyle": True,
+        "storage-s3-multipartcopythresholdsize": 100500,
+    }
+    expected_storage = {
+        "s3": {
+            "bucket": "test_bucket",
+            "region": "ns1",
+            "forcepathstyle": True,
+            "encrypt": False,
+            "secure": True,
+            "skipverify": False,
+            "v4auth": True,
+            "chunksize": 10485760,
+            "multipartcopychunksize": 31457280,
+            "multipartcopymaxconcurrency": 100,
+            "multipartcopythresholdsize": 100500,
+            "storageclass": "STANDARD",
+            "usedualstack": False,
+            "accelerate": False,
+            "loglevel": "off",
+        },
+    }
+    with mock.patch("charms.layer.docker_registry.yaml") as mock_yaml:
+        layer.docker_registry.configure_registry()
+        args, _ = mock_yaml.safe_dump.call_args_list[0]
+        assert 'storage' in args[0]
+        assert 's3' in args[0]['storage']
+        actual_storage_config = args[0]['storage']['s3']
+        assert expected_storage['s3'].items() == actual_storage_config.items()
+
+
+@mock.patch("os.makedirs", mock.Mock(return_value=0))
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry._configure_local_client")
+@mock.patch("charms.layer.docker_registry._write_tls_blobs_to_files")
+@mock.patch("charms.layer.docker_registry.unitdata")
+@mock.patch("charmhelpers.core.hookenv.config")
+def test_configure_registry_default_file_storage(config, *args):
+    config.return_value = {
+        "log-level": "info"
+    }
+    expected_storage = {
+        "filesystem": {
+            "rootdirectory": "/var/lib/registry"
+        }
+    }
+    with mock.patch("charms.layer.docker_registry.yaml") as mock_yaml:
+        layer.docker_registry.configure_registry()
+        args, _ = mock_yaml.safe_dump.call_args_list[0]
+        assert 'storage' in args[0]
+        assert 'filesystem' in args[0]['storage']
+        actual_storage_config = args[0]['storage']['filesystem']
+        assert expected_storage['filesystem'].items() == actual_storage_config.items()
