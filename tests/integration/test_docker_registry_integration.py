c72417f2c23fae8b9d62e4dc92deea81cd39ee33
Adam Dyess <adam.dyess@canonical.com>
2025-04-02 13:36:36 -0500

    layers and interfaces pull from charmed-kubernetes gh org branch

--- tests/integration/test_docker_registry_integration.py
+++ tests/integration/test_docker_registry_integration.py
@@ -8,7 +8,7 @@ log = logging.getLogger(__name__)
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test : OpsTest, series):
+async def test_build_and_deploy(ops_test: OpsTest, series):
     """Build and deploy docker-registry in bundle"""
     charm = next(Path.cwd().glob("docker-registry*.charm"), None)
     if not charm:
