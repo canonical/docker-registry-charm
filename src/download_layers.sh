f8b62038335aefdfc18ab9d6090ebf75349e60ce
Adam Dyess <adam.dyess@canonical.com>
2025-04-04 12:15:43 -0500

    Finalize 'download_layers.sh'

--- src/download_layers.sh
+++ src/download_layers.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-# This script is used to prepare the layers for the charm build process.
-# It will pull the layers from the charm store and place them in the tmp directory.
+# This script is used to download reactive layers for the charm build process.
+# It pulls the layers from the gh and place them in the tmp directory.
 set -eu
 
 # ensure we have environment variables set
 : "${RELEASE_BRANCH:?Environment variable RELEASE_BRANCH is not set.}"
 : "${CHARM_LAYERS_DIR:?Environment variable CHARM_LAYERS_DIR is not set.}"
 : "${CHARM_INTERFACES_DIR:?Environment variable CHARM_INTERFACES_DIR is not set.}"
-: "${CRAFT_ARCH_BUILD_ON:=$(dpkg --print-architecture)}"
-: "${LAYER_INDEX:='https://raw.githubusercontent.com/charmed-kubernetes/layer-index/main/'}"
 
 # Ensure the temporary directory was created successfully
 TMP_DIR=$(mktemp -d)
@@ -26,14 +24,14 @@ for cmd in charm wget yq; do
     fi
 done
 
-mkdir -p $CHARM_INTERFACES_DIR
-mkdir -p $CHARM_LAYERS_DIR
+rm -rf $CHARM_LAYERS_DIR && mkdir -p $CHARM_LAYERS_DIR
+rm -rf $CHARM_INTERFACES_DIR && mkdir -p $CHARM_INTERFACES_DIR
 wget "https://raw.githubusercontent.com/charmed-kubernetes/jenkins/refs/heads/main/jobs/includes/charm-layer-list.inc" -O ${TMP_DIR}/layers.yaml
 for layer in $(cat ${TMP_DIR}/layers.yaml | yq '.[] | keys | .[]' - ); do
     if [[ ! "$layer" =~ ^(interface:|layer:) ]]; then continue; fi
     echo "Pulling layer: ${layer}"
     charm pull-source \
-        --layer-index ${LAYER_INDEX} \
+        --layer-index ${LAYER_INDEX:-'https://raw.githubusercontent.com/charmed-kubernetes/layer-index/main/'} \
         --branch ${RELEASE_BRANCH} \
         ${layer} &
 done
